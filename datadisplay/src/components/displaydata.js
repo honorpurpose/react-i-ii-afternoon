05c1049f1d0502775faf46d3d8967a24d7c4113b
unknown <honorpurpose@gmail.com>
2019-04-12 13:29:42 -0600

    completed 04122019

--- datadisplay/src/components/displaydata.js
+++ datadisplay/src/components/displaydata.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import data from './data'
+
 import './displaydata.css'
 
 class DisplayData extends Component {
     constructor(props){
         super(props);
+
+        
+
     }
+
+    
+    
 
     render(){
         return (
@@ -13,19 +19,19 @@ class DisplayData extends Component {
                 <div className='home'>Home</div>
                 
                     <div className='displayContent'>
-                        <h1 className='whereAt'>{ this.props.index + 1 }/25</h1>
-                        <h1 className='firstLastName'>{data[this.props.index].name.first} {data[this.props.index].name.last}</h1>
+                        <h1 className='whereAt'>{ this.props.index + 1 }/{this.props.data.length}</h1>
+                        <h1 className='firstLastName'>{this.props.data[this.props.index].name.first} {this.props.data[this.props.index].name.last}</h1>
                         <div className='personInfo'>
-                            <p><strong>From: </strong> { data[this.props.index].city }, { data[this.props.index].country }</p>
-                            <p><strong>Job Title: </strong> { data[this.props.index].title }</p>
-                            <p><strong>Employer: </strong>{ data[this.props.index].employer }</p>
+                            <p><strong>From: </strong> { this.props.data[this.props.index].city }, { this.props.data[this.props.index].country }</p>
+                            <p><strong>Job Title: </strong> { this.props.data[this.props.index].title }</p>
+                            <p><strong>Employer: </strong>{ this.props.data[this.props.index].employer }</p>
                         </div>
                         <div className='movieContainer'>
                             <p><strong>Favorite Movies:</strong></p>
                             <div className='favoriteMovies'>
-                                <p>1. { data[this.props.index].favoriteMovies[0] }</p>
-                                <p>2. { data[this.props.index].favoriteMovies[1] }</p>
-                                <p>3. { data[this.props.index].favoriteMovies[2] }</p>
+                                <p>1. { this.props.data[this.props.index].favoriteMovies[0] }</p>
+                                <p>2. { this.props.data[this.props.index].favoriteMovies[1] }</p>
+                                <p>3. { this.props.data[this.props.index].favoriteMovies[2] }</p>
                             </div>
                         </div>
                     </div>
--- datadisplay/src/components/stateholder.js
+++ datadisplay/src/components/stateholder.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import DisplayData from './displaydata';
 import './stateholder.css'
+import data from './data'
+
+
 
 class StateHolder extends Component {
     constructor(props){
@@ -8,14 +11,16 @@ class StateHolder extends Component {
 
         this.state = {
             index: 0,
+            data: data
         }
 
         this.handleNextClick = this.handleNextClick.bind( this )
         this.handleBackClick = this.handleBackClick.bind( this )
+        
     }
     
     handleNextClick(){
-        if ( this.state.index < 24) {
+        if ( this.state.index < this.state.data.length - 1) {
             this.setState({
                 index: this.state.index + 1
             })
@@ -35,23 +40,34 @@ class StateHolder extends Component {
             })
         } else {
             this.setState({
-                index: 24
+                index: this.state.data.length - 1
             })
-        }
+        }   
+    }
 
+    
 
-        
+    handleDeleteButton(index){
+        let newArray = data.splice(index, 1)
+        this.setState({
+            data: data,
+            
+        })
+        console.log(this.state.data)
     }
 
     render(){
         let nextButton = 'Next >';
         let backButton = '< Previous'
+        
         return(
             <div>
-                <DisplayData index={ this.state.index } />
+                <DisplayData index={ this.state.index } data={ this.state.data } />
                 <div className='buttonDiv'>
                     <div className='backDiv' onClick={ this.handleBackClick }> { backButton } </div>
+                    <div className='deleteDiv' onClick={ () => this.handleDeleteButton(this.state.index) }> Delete </div>
                     <div className='nextDiv' onClick={ this.handleNextClick }> { nextButton } </div>
+        
                 </div>
                 
                 
